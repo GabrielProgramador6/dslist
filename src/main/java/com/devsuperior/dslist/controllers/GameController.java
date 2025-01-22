38643eafe7092e73eb0981e8b29a18f7ed9e921c
Gabriel de Oliveira Freitas <gabrielProgramador6@gmail.com>
2025-01-22 11:48:40 -0300

    Game and List

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,11 @@ public class GameController {
     @GetMapping
     public ResponseEntity<List<GameMinDTO>> findAll() {
         return ResponseEntity.ok(gameService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GameDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(gameService.findById(id));
     }
 
 }
---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/lists")
+public class GameListController {
+
+    private final GameListService gameListService;
+
+    private final GameService gameService;
+
+    public GameListController(GameListService gameListService, GameService gameService) {
+        this.gameListService = gameListService;
+        this.gameService = gameService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GameListDTO>> findAll() {
+        return ResponseEntity.ok(gameListService.findAll());
+    }
+
+    @GetMapping("/{listId}/games")
+    public ResponseEntity<List<GameMinDTO>> findByList(@PathVariable Long listId) {
+        return ResponseEntity.ok(gameService.findByList(listId));
+    }
+
+}
